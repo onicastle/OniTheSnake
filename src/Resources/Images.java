f41068b1ba3bb4abfc684d43559eafa51b748e7e
Zulmarie Jimenez <zulmariejimenez@10.34.0.124>
2018-09-27 19:20:48 -0400

    image change

--- src/Resources/Images.java
+++ src/Resources/Images.java
@@ -28,7 +28,7 @@ public class Images {
 
         try {
 
-            title = ImageIO.read(getClass().getResourceAsStream("/Sheets/Title.png"));
+            title = ImageIO.read(getClass().getResourceAsStream("/Sheets/Snake.png"));
             Pause = ImageIO.read(getClass().getResourceAsStream("/Buttons/Pause.png"));
             Resume[0] = ImageIO.read(getClass().getResourceAsStream("/Buttons/Resume.png"));
             Resume[1] = ImageIO.read(getClass().getResourceAsStream("/Buttons/ResumeP.png"));
